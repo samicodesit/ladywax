d39ff41d8257178760ec5f1cd1c84824383f1c6f
Sami Shata <samicodesit@gmail.com>
2026-01-26 11:28:02 +0100

    update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ const lato = Lato({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.ladywax.nl"),
   title: `${siteConfig.name} - ${siteConfig.description}`,
   description: siteConfig.description,
   keywords: [
@@ -34,8 +35,24 @@ export const metadata: Metadata = {
   openGraph: {
     title: siteConfig.name,
     description: siteConfig.description,
+    url: "https://www.ladywax.nl",
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: "/images/logo.png",
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
     type: "website",
     locale: "nl_NL",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: ["/images/logo.png"],
   },
   icons: {
     icon: "/favico/favicon.ico",
